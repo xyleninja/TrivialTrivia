0c82cc6c8ae811cc90844e75cf43b9099915e11e
Sebastian Strubert <sebi@strubert.de>
2018-07-09 16:30:49 +0200

    Updated the welcome Page and created the search lobby page

---
+++ TrivialTrivia/TrivialTrivia/TrivialTrivia/View/P_Lobby.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TrivialTrivia
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class P_Lobby : ContentPage
+	{
+		public P_Lobby ()
+		{
+			InitializeComponent ();
+		}
+	}
+}
