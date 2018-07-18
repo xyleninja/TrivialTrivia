2ec27a3ae4a2bb19e4dcb6420139f2ce76b92f60
Sebastian Strubert <sebi@strubert.de>
2018-07-18 17:10:18 +0200

    magic-> udp is working with some minor mistakes

---
+++ TrivialTrivia/TrivialTrivia/TrivialTrivia/Model/Lobby.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace TrivialTrivia
+{
+    public class Lobby
+    {
+        public string name { get; set; }
+        public QuestionSet questionSet { get; set; }
+        public int numberOfQuestions { get; set; }
+        public bool hasHints { get; set; }
+        public int transitionInSeconds { get; set; }
+        public int secondsPerQuestion { get; set; }
+        public int maximumPlayers { get; set; }
+
+        public List<Player> players { get; set; }
+    }
+}
---
+++ TrivialTrivia/TrivialTrivia/TrivialTrivia/ViewModel/SearchLobbyViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+
+namespace TrivialTrivia
+{
+    public class SearchLobbyViewModel : BaseViewModel
+    {
+        private bool p_isBusy;
+
+        public ObservableCollection<HostingLobbyInformation> hostingLobbies { get; set; } = new ObservableCollection<HostingLobbyInformation>();
+        public bool isBusy
+        {
+            get { return p_isBusy; }
+            set
+            {
+                p_isBusy = value;
+                OnPropertyChanged("isBusy");
+            }
+        }
+    }
+}
